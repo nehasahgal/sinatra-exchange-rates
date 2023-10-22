3092d210c2356df36d015a37fd9423cd930d21d3
nehasahgal <146212872+nehasahgal@users.noreply.github.com>
2023-10-22 16:53:43 +0000

    homepage and subpage made dynamic

--- app.rb
+++ app.rb
@@ -11,3 +11,9 @@ get("/:country_one") do
   erb(:country_one)
   
 end
+
+get("/:country_one/:country_two") do
+  @country_one = params.fetch("country_one")
+  @country_two = params.fetch("country_two")
+  erb(:result)
+end
